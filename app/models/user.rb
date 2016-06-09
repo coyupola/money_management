4c9973315fe596f549726f7fb6e29b27ffee8aa6
coyupola <yuka@kamiyamayuka-no-MacBook-Air.local>
2016-06-09 14:48:08 +0900

    no message

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,7 @@ class User < ActiveRecord::Base
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  
-  　#accessor
-  attr_accessor :group_key
+
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable,
